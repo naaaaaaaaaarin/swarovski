0e4352baaf6a576a31e666dc7474862451a79a66
naaaaaaaaaarin <89621035+naaaaaaaaaarin@users.noreply.github.com>
2022-06-14 09:27:22 +0900

    Add files via upload

---
+++ js/rwd_template.js
@@ -0,0 +1,21 @@
+// 모바일메뉴 스크립트
+$('.menu_toggle_btn').click(function(){
+    $('header .gnb').stop().slideToggle(300);
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
